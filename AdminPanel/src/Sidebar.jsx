158571473d34731aa74b0341f42bf2f3e42b0ffd
Tharique Afridi <tharique@visiblestarsksa.com>
2023-11-22 15:00:06 +0300

    First commit

---
+++ AdminPanel/src/Sidebar.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import 
+{BsCart3, BsGrid1X2Fill, BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill, 
+  BsListCheck, BsMenuButtonWideFill, BsFillGearFill, BsAppIndicator, BsPersonCheckFill, BsReceipt, BsChatDotsFill}
+ from 'react-icons/bs'
+
+function Sidebar({openSidebarToggle, OpenSidebar}) {
+  return (
+    <aside id="sidebar" className={openSidebarToggle ? "sidebar-responsive": ""}>
+        <div className='sidebar-title'>
+            <div className='sidebar-brand'>
+                <BsAppIndicator  className='icon_header'/> ADMIN PANEL
+            </div>
+            <span className='icon close_icon' onClick={OpenSidebar}>X</span>
+        </div>
+
+        <ul className='sidebar-list'>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsGrid1X2Fill className='icon'/> Surveys
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsFillArchiveFill className='icon'/> Products
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsChatDotsFill className='icon'/> Channels
+                </a>
+            </li>
+           
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsPersonCheckFill className='icon'/> Profile
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsReceipt className='icon'/> Reports
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsFillGearFill className='icon'/> Settings
+                </a>
+            </li>
+        </ul>
+    </aside>
+  )
+}
+
+export default Sidebar
